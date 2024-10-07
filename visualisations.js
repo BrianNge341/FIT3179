9e3af902a107823f2a99cff52eed7aeaffd47d7a
Brian Nge <brianjinghong@gmail.com>
2024-10-07 23:54:23 +1100

    improved the website

--- visualisations.js
+++ visualisations.js
@@ -27,29 +27,44 @@ document.addEventListener('DOMContentLoaded', function() {
     }).then(function(result) {
     }).catch(console.error);
 
-    var vg_heatmap = "visualisation/heatmap_visualisation.vg.json";
-    vegaEmbed("#heatmap", vg_heatmap, {
+    // var vg_heatmap = "visualisation/heatmap_visualisation.vg.json";
+    // vegaEmbed("#heatmap", vg_heatmap, {
+    //     renderer: "svg",
+    //     actions: false
+    // }).then(function(result) {
+    // }).catch(console.error);
+
+    // var vg_sun = "visualisation/sunburst_visualisation.vg.json";
+    // vegaEmbed("#sun", vg_sun, {
+    //     renderer: "svg",
+    //     actions: false
+    // }).then(function(result) {
+    // }).catch(console.error);
+
+    // var vg_bubble = "visualisation/bubble_visualisation.vg.json";
+    // vegaEmbed("#bubble", vg_bubble, {
+    //     renderer: "svg",
+    //     actions: false
+    // }).then(function(result) {
+    // }).catch(console.error);
+
+    // var vg_treemap = "visualisation/treemap_visualisation.vg.json";
+    // vegaEmbed("#treemap", vg_treemap, {
+    //     renderer: "svg",
+    //     actions: false
+    // }).then(function(result) {
+    // }).catch(console.error);
+
+    var vg_linechart = "visualisation/linechart_visualisation.vg.json";
+    vegaEmbed("#linechart", vg_linechart, {
         renderer: "svg",
         actions: false
     }).then(function(result) {
     }).catch(console.error);
 
-    var vg_sun = "visualisation/sunburst_visualisation.vg.json";
-    vegaEmbed("#sun", vg_sun, {
-        renderer: "svg",
-        actions: false
-    }).then(function(result) {
-    }).catch(console.error);
 
-    var vg_bubble = "visualisation/bubble_visualisation.vg.json";
-    vegaEmbed("#bubble", vg_bubble, {
-        renderer: "svg",
-        actions: false
-    }).then(function(result) {
-    }).catch(console.error);
-
-    var vg_treemap = "visualisation/treemap_visualisation.vg.json";
-    vegaEmbed("#treemap", vg_treemap, {
+    var vg_barchart = "visualisation/barchart_visualisation.vg.json";
+    vegaEmbed("#barchart", vg_barchart, {
         renderer: "svg",
         actions: false
     }).then(function(result) {
